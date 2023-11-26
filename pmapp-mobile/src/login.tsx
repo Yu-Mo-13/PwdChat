d871ffbd7ca13ca4bcabaf423050839ab25f7b70
Yuya Motozono <motozono.tech@gmail.com>
2023-11-26 19:37:39 +0900

    環境変数切り戻し

--- pmapp-mobile/src/login.tsx
+++ pmapp-mobile/src/login.tsx
@@ -24,8 +24,8 @@ const Login = () => {
             await LoginSchema.validate({ userId, keyword });
 
             // ユーザーIDとパスワードを照合する
-            const id: string = import.meta.env.VITE_USER;
-            const pass: string = import.meta.env.VITE_KEYWORD;
+            const id: string = atob(import.meta.env.VITE_USER);
+            const pass: string = atob(import.meta.env.VITE_KEYWORD);
             if (!(userId === id && keyword === pass)) {
                 alert("ログイン情報が間違っています。");
                 return;
