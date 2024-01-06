24496a6e042f65a5c1f4b2cc46f3c9c9a332b86c
Yuya Motozono <motozono.tech@gmail.com>
2024-01-06 14:01:58 +0900

    ts-node -> ts-node-esmに変更

--- server.ts
+++ server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'build')));
@@ -10,10 +12,12 @@ app.use(express.static(path.join(__dirname, '..', 'build')));
 // 静的ファイルのルーティング
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'build'));
+    console.log(req);
 });
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'build/index.html'));
+    console.log(req);
 });
 
 // サーバー起動
