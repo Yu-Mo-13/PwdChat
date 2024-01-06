2d49d3a2dfb0e342f7451c82293358b715c27ed4
Yuya Motozono <motozono.tech@gmail.com>
2024-01-06 16:48:50 +0900

    本番デプロイ対応(3)

--- src/TextBoxFunction.tsx
+++
@@ -1,6 +0,0 @@
-// テキストボックスから入力された値を取得する
-function getText(inputId: string) {
-    const input = document.getElementById(inputId) as HTMLInputElement;
-    const text: string = input.value;
-    return text;
-}
